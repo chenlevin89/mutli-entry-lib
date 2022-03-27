a724b7b4605a30d02cda2090d1014efae0091bff
chen.levin <chen.levin@ironsrc.com>
2022-03-27 14:11:44 +0300

    stable

---
+++ projects/awesome/src/public-api.ts
@@ -0,0 +1,8 @@
+export * from './lib/components/a/a.module'
+export * from './lib/components/a/a.component'
+
+export * from './lib/components/b/b.module'
+export * from './lib/components/b/b.component'
+
+export * from './lib/components/c/c.module'
+export * from './lib/components/c/c.component'
